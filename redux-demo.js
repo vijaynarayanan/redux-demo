73eb6c58e84b86c7386f3284399a6b4aa57aa0c7
Vijay Narayanan <vijay.narayanan25@gmail.com>
2021-12-13 18:01:46 -0500

    first commit

---
+++ redux-demo.js
@@ -0,0 +1,28 @@
+const redux = require("redux");
+
+const defaultState = { counter: 0 };
+
+const counterReducer = (state = defaultState, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
+};
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
